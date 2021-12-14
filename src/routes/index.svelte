18fe93442ebaa5d73b1b1cd58f54b7a413e2483c
Michael Bromley <michael@michaelbromley.co.uk>
2021-12-14 15:13:26 +0100

    Remove unused styles from index

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -32,31 +32,6 @@
 </svelte:head>
 
 <ComparisonTable {table}></ComparisonTable>
+
 <style>
-    section {
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-        flex: 1;
-    }
-
-    h1 {
-        width: 100%;
-    }
-
-    .welcome {
-        position: relative;
-        width: 100%;
-        height: 0;
-        padding: 0 0 calc(100% * 495 / 2048) 0;
-    }
-
-    .welcome img {
-        position: absolute;
-        width: 100%;
-        height: 100%;
-        top: 0;
-        display: block;
-    }
 </style>
