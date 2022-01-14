b518a49dec1db57bbae3f47e6cc2d866e6937db8
Daniel <daniel.salvado@gmail.com>
2022-01-14 20:33:51 +0000

    Add Crystallize platform

--- src/routes/table-data.json.ts
+++ src/routes/table-data.json.ts
@@ -4,13 +4,14 @@ import vendure from '../solutions/vendure';
 import medusa from '../solutions/medusa';
 import sylius from '../solutions/sylius';
 import saleor from '../solutions/saleor';
+import crystallize from '../solutions/crystallize';
 
 export async function get({ params }) {
 
 
-    return {
+  return {
         body: {
-            data: [vendure, saleor, sylius, medusa]
+            data: [vendure, saleor, sylius, medusa, crystallize]
         }
     };
 }
---
+++ src/solutions/crystallize.ts
@@ -0,0 +1,149 @@
+import type { SelfHostedFrameworkInfo } from '../types';
+
+const template: SelfHostedFrameworkInfo = {
+    name: 'Crystallize',
+    website: 'https://crystallize.com/',
+    type: 'selfHosted',
+    language: { value: 'TypeScript' },
+    license: { value: '' },
+    underlyingFramework: { value: 'NestJS' },
+    supportedDatabases: { value: ['MongoDB'] },
+    storefrontApi: { value: ['graphql'] },
+    adminApi: { value: ['graphql'] },
+    productVariants: {
+        value: { support: 'full' },
+        note: '',
+        urls: ['https://crystallize.com/learn/concepts/pim/product']
+    },
+    configurableProducts: { value: { support: 'full' }, note: '', urls: [] },
+    bundleProducts: { value: { support: 'full' }, note: '', urls: [] },
+    stockTracking: {
+        value: { support: 'full' },
+        note: '',
+        urls: ['https://crystallize.com/blog/managing-stock-locations-in-crystallize']
+    },
+    multiWarehouse: {
+        value: { support: 'full' },
+        note: '',
+        urls: ['https://crystallize.com/blog/managing-stock-locations-in-crystallize']
+    },
+    productSearch: {
+        value: { support: 'full' },
+        note: '',
+        urls: ['https://crystallize.com/learn/developer-guides/search-api']
+    },
+    facetedSearch: {
+        value: { support: 'full' },
+        note: '',
+        urls: [
+            'https://crystallize.com/learn/developer-guides/search-api',
+            'https://crystallize.com/learn/developer-guides/search-api/search-products-by-attributes'
+        ]
+    },
+    productCategories: {
+        value: { support: 'full' },
+        note: '',
+        urls: ['https://crystallize.com/blog/product-taxonomy-using-topic-maps']
+    },
+    customAttributes: {
+        value: { support: 'full' },
+        note: '',
+        urls: [
+            'https://crystallize.com/learn/developer-guides/catalogue-api/fetching-an-item/components'
+        ]
+    },
+    tieredPricing: {
+        value: { support: 'full' },
+        note: '',
+        urls: ['https://crystallize.com/learn/concepts/ecommerce/price-variant']
+    },
+    assetManagement: { value: { support: 'full' }, note: '', urls: [] },
+    multiStore: { value: { support: 'full' }, note: '', urls: [] },
+    multiCurrency: {
+        value: { support: 'full' },
+        note: '',
+        urls: ['https://crystallize.com/learn/concepts/ecommerce/currency']
+    },
+    multiLanguage: {
+        value: { support: 'full' },
+        note: '',
+        urls: ['https://crystallize.com/learn/concepts/pim/multilingual']
+    },
+    promotions: {
+        value: { support: 'full' },
+        note: 'Full support for custom-built solution',
+        urls: ['https://crystallize.com/learn/livestreams/shop-examples/gift-cards-and-vouchers']
+    },
+    voucherCodes: {
+        value: { support: 'full' },
+        note: 'Full support for custom-built solution',
+        urls: ['https://crystallize.com/learn/livestreams/shop-examples/gift-cards-and-vouchers']
+    },
+    freeShipping: { value: { support: 'full' }, note: '', urls: [] },
+    giftCards: {
+        value: { support: 'full' },
+        note: 'Full support for custom-built solution',
+        urls: ['https://crystallize.com/learn/livestreams/shop-examples/gift-cards-and-vouchers']
+    },
+    guestCheckout: { value: { support: 'full' }, note: '', urls: [] },
+    orderProcessIsConfigurable: {
+        value: { support: 'full' },
+        note: '',
+        urls: ['https://crystallize.com/learn/developer-guides/order-api/fulfilment-pipelines']
+    },
+    adminCreatedOrders: {
+        value: { support: 'partial' },
+        note: '',
+        urls: [
+            'https://crystallize.com/learn/developer-guides/order-api/creating-orders',
+            'https://crystallize.com/learn/developer-guides/order-api'
+        ]
+    },
+    multipleFulfillments: {
+        value: { support: 'full' },
+        note: '',
+        urls: [
+            'https://crystallize.com/learn/developer-guides/order-api/fulfilment-pipelines',
+            'https://crystallize.com/learn/user-guides/orders-and-fulfilment'
+        ]
+    },
+    returns: { value: { support: 'full' }, note: '', urls: [] },
+    invoiceGeneration: {
+        value: { support: 'partial' },
+        note: 'Full support for custom-built solution',
+        urls: []
+    },
+    customerAccounts: {
+        value: { support: 'partial' },
+        note: 'Full support for custom-built solution',
+        urls: []
+    },
+    customerMultipleAddresses: {
+        value: { support: 'full' },
+        note: 'Full support for custom-built solution',
+        urls: []
+    },
+    customerOrderHistory: { value: { support: 'full' }, note: '', urls: [] },
+    customerGroups: { value: { support: 'full' }, note: '', urls: [] },
+    thirdPartyAuth: {
+        value: { support: 'full' },
+        note: '',
+        urls: ['https://crystallize.com/learn/developer-guides/api-overview/authentication']
+    },
+    wishlists: { value: { support: 'partial' }, note: '', urls: [] },
+    adminInterface: { value: { support: 'full' }, note: '', urls: [] },
+    sso: {
+        value: { support: 'full' },
+        note: '',
+        urls: ['https://crystallize.com/learn/developer-guides/access-tokens']
+    },
+    adminInterfaceIsThemeable: {
+        value: { support: 'partial' },
+        note: 'Full support for custom-built solution',
+        urls: []
+    },
+    adminInterfaceLocalizable: { value: { support: 'none' }, note: '', urls: [] },
+    roleBasedAccessControl: { value: { support: 'none' }, note: '', urls: [] }
+};
+
+export default template;
